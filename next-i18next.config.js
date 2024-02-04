4468e63d60bb68e207136dfbe6ce3a9d0e75fab0
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2024-02-04 17:39:16 +0400

    i18n-bug

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next-i18next').UserConfig} */
+
+const path = require('path');
+
 module.exports = {
+    debug: process.env.NODE_ENV === 'development',
     i18n: {
       defaultLocale: 'en',
       locales: ['en'],
       localeDetection: false
     },
+    localePath: path.resolve('./public/locales'),
+    reloadOnPrerender: process.env.NODE_ENV === 'development',
 }
