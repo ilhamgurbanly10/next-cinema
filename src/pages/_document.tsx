a88bbefb161553d3a1dd1f4e4da7e68c677f2355
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2024-02-04 15:24:18 +0400

    c-favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link rel="stylesheet" href="/styles/globallll.css" />
+          <link rel="icon" href="/favicon.png" type="image/x-icon" />
         </Head>
         <body>
           <Main />
