098081b1ba5c3e49c2557174f80c3ca2b920f484
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2024-02-04 15:30:24 +0400

    c-build-bug-01

--- src/shared/state/selectors.ts
+++ src/shared/state/selectors.ts
@@ -1,18 +1,11 @@
 import { selector } from 'recoil';
-import { userState, infoState, loadingState } from './atoms';
-import {UserAtom, InfoAtom} from './type';
+import { infoState, loadingState } from './atoms';
+import {InfoAtom} from './type';
 
 export const loadingSelector = selector({
   key: 'loadingSelector',
   get: ({ get }) => {
     return get<boolean>(loadingState);
-  },
-});
-
-export const userSelector = selector({
-  key: 'userSelector',
-  get: ({ get }) => {
-    return get<UserAtom>(userState);
   },
 });
 
